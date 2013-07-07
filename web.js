9aa0bf1702a103d71e524390e2308c29af973a26
Terry Wong <twongter@gmail.com>
2013-07-07 14:51:35 +0000

    read welcome msg from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexBuf = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
