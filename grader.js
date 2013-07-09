7f9fe61edf29adf9e1a99050e767e8589f034131
Terry Wong <twongter@gmail.com>
2013-07-09 15:57:50 +0000

    enhance grader to check pages on web

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +62,37 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var doCheck = function(file, check_list) {
+    var checkJson = checkHtmlFile(file, check_list);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+var buildfn = function(file, check_list) {
+    var response2doCheck = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            doCheck(file, check_list);
+        }
+    };
+    return response2doCheck;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+        console.log('get from web');
+        var response2doCheck = buildfn(program.file, program.checks);
+        rest.get(program.url).on('complete', response2doCheck);
+    } else {
+        console.log('get from local');
+        doCheck(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
